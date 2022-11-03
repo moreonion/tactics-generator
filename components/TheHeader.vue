89bbe1c9cfc1756c19392e011d4d5d17c227db98
Maya Kokits <m.kokits@posteo.de>
2022-11-03 16:51:07 +0100

    Update links.

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,7 +4,7 @@
       <div id="my-header" class="title-bar hide-for-large">
         <div class="title-bar-left">
           <ul class="menu">
-            <li><a class="logo" href="https://www.impact-stack.org/"><img class="logo" src="https://act.more-onion.com/profiles/actmo/themes/impact_stack_theme/logo.png" alt="Logo More Onion"></a></li>
+            <li><a class="logo" href="https://www.impact-stack.org/" target="_blank"><img class="logo" src="https://act.more-onion.com/profiles/actmo/themes/impact_stack_theme/logo.png" alt="Logo More Onion"></a></li>
           </ul>
         </div>
         <div class="title-bar-right">
@@ -15,9 +15,9 @@
     <div class="grid-container">
       <div class="grid-x grid-margin-x">
         <div id="menu-desktop" class="cell top-bar show-for-large">
-          <a class="logo" href="https://www.impact-stack.org/"><img class="logo top-bar-left" src="https://act.more-onion.com/profiles/actmo/themes/impact_stack_theme/logo.png" alt="Logo More Onion"> </a>
+          <a class="logo" href="https://www.impact-stack.org/" target="_blank"><img class="logo top-bar-left" src="https://act.more-onion.com/profiles/actmo/themes/impact_stack_theme/logo.png" alt="Logo More Onion"> </a>
           <div class="top-bar-right">
-            <a href="#">About</a>
+            <a href="https://www.impact-stack.org/the-campaign-tactics-generator/" target="_blank">About</a>
           </div>
         </div>
       </div>
