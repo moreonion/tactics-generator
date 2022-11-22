1832abcda9c6a21633e9bcd4fb059589b389ec8f
Maya Kokits <m.kokits@posteo.de>
2022-11-22 15:11:07 +0100

    Add intro block, change background image.

--- layouts/twoColumn.vue
+++ layouts/twoColumn.vue
@@ -4,7 +4,14 @@
     <TheHeader />
 
     <section id="main">
-      <h1 class="show-for-sr">Advocacy campaign tactic idea generator by Impact Stack</h1>
+      <div class="grid-container">
+        <div class="grid-x grid-margin-x">
+          <div class="cell medium-9 large-7 intro">
+            <h1>The advocacy campaign tactic idea generator</h1>
+            <p>Successful advocacy campaigns need a good strategy and creative ideas for tactics. Our idea generator can help you spark new ideas or reshape old ones. <a href="https://www.impact-stack.org/the-campaign-tactics-generator/" title="More about the Impact Stack tactics generator">Find out more</a> or click the "Generate new tactic" button to explore.</p>
+          </div>
+        </div>
+      </div>
       <div class="grid-container">
         <div class="grid-x grid-margin-x">
 
@@ -31,8 +38,17 @@
 <style lang="scss">
 #main {
   overflow: initial; // To make position: sticky work on the sidebar.
-  background-image: url(https://act.more-onion.com/sites/actmo/files/styles/banner/public/background_dark_image.png?itok=4fDMy_b9);
+  background-image: url(https://ucarecdn.com/415f1900-5ecc-4b2c-a8db-9dfef91e4981/background__green_people_flipped.png);
   background-repeat: no-repeat;
   background-size: cover;
+
+  @media print, screen and (max-width: 63.999em) {
+    background-position: bottom right;
+    background-size: auto;
+  }
+
+  .intro {
+    padding-bottom: 3.3rem;
+  }
 }
 </style>
