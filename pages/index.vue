557363c69bdeb382e4027ff4a7d48943beff95f3
Maya Kokits <m.kokits@posteo.de>
2023-08-01 19:56:23 +0200

    chore: remove unused composable

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,6 @@ import { getRandomSlugs } from '@/utils/utils'
 
 // Get a pair of random slugs from all the available ones.
 const { pending, data } = await useFetch('/api/slugs', {key: 'slugs'});
-// const { data } = await useAvailableSlugs()
 const { randomTactic, randomHook } = getRandomSlugs(data.value);
 
 if (process.client) {
